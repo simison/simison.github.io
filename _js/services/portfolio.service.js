6f1fc9277f0b3c6ca11d96f4a53c346393cd89d9
Mikael Korpela <mikael@ihminen.org>
2016-08-27 14:06:40 +0300

    Update portfolio.service.js

--- _js/services/portfolio.service.js
+++ _js/services/portfolio.service.js
@@ -2,48 +2,6 @@
  * Portfolio Factory
  * @namespace Factories
  */
-//(function() {
-//  'use strict';
-//
-//  angular
-//    .module('app')
-//    .factory('PortfolioService', PortfolioService);
-//
-//  /**
-//   * @namespace PortfolioService
-//   * @desc Portfolio service used returning portfolio contents
-//   * @memberOf Factories
-//   */
-//  /* @ngInject */
-//  function PortfolioService($http) {
-//
-//    var service = {
-//      get: getPortfolio
-//    };
-//
-//    return service;
-//
-//    function getPortfolio() {
-//      return $http.get('/portfolio/contents.json')
-//          .then(getPortfolioComplete)
-//          .catch(function(message) {
-//            exception.catcher('XHR Failed for PortfolioService->get()')(message);
-//          });
-//      function getPortfolioComplete(data, status, headers, config) {
-//        console.log('->getPortfolioComplete');
-//        console.log(data, status, headers, config);
-//        return data.works;
-//      }
-//    }
-//
-//  }
-//
-//})();
-
-/**
- * Bookmarks Factory
- * @namespace Factories
- */
 (function() {
   'use strict';
 
@@ -52,8 +10,8 @@
     .factory('PortfolioService', PortfolioService);
 
   /**
-   * @namespace BookmarksService
-   * @desc Bookmarks service used for communicating with the REST endpoint
+   * @namespace PortfolioService
+   * @desc Portfolio service used for communicating with the REST endpoint
    * @memberOf Factories
    */
   /* @ngInject */
@@ -73,7 +31,7 @@
         }
 
         function getPortfolioFailed(error) {
-            $log.error('XHR Failed for getAvengers.' + error.data);
+            $log.error('XHR Failed for portfolio.' + error.data);
         }
     }
   }
